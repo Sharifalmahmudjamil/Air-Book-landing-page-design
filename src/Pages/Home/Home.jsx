18b8fd713e11f29ff04394c9fb7feeafc6ebee94
sharifalmahmudjamil <smjamil111@gmail.com>
2024-03-07 13:21:00 -1200

    website landing page design travel blog and subscribe section are completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import Banner from "../../Banner/Banner";
 import Featured from "../Featured/Featured";
+import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import OffersSection from "../OffersSection/OffersSection";
 import PopularAirlines from "../PopularAirlines/PopularAirlines";
+import SubscribeSection from "../SubscribeSection/SubscribeSection";
 import Testimonials from "../Testimonials/Testimonials";
 import TravelBlog from "../TravelBlog/TravelBlog";
 
@@ -17,6 +19,8 @@ const Home = () => {
             <PopularAirlines></PopularAirlines>
             <Testimonials></Testimonials>
             <TravelBlog></TravelBlog>
+            <SubscribeSection></SubscribeSection>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/SubscribeSection/SubscribeSection.jsx
+++ src/Pages/SubscribeSection/SubscribeSection.jsx
@@ -1,9 +1,19 @@
-
+import image1 from "../../../public/images/Isolation_Mode.png"
 
 const SubscribeSection = () => {
     return (
-        <div>
-            
+        <div className="max-w-screen-xl mx-auto mt-10 overflow-x-hidden">
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <img className="w-[340px] h-[340px] lg:mx-10" src={image1} alt="" />
+            </div>
+            <div className="mt-10 lg:-mx-11">
+                <h1 className="text-4xl font-extrabold">Subscribe to our Newsletter!</h1>
+                <p className="text-xl font-medium text-blue-500 mt-5">Subscribe to our newsletter and stay updated.</p>
+                <input className="lg:w-[500px] h-[50px] border border-blue-500 rounded-2xl mt-5" type="email" name="" id="" placeholder="Your Email"/>
+                <button className="btn btn-warning lg:w-[500px] rounded-2xl text-xl font-medium mt-5">Subscribe</button>
+            </div>
+            </div>
         </div>
     );
 };
